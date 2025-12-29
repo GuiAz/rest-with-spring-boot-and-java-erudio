348dcc4b7a54fca0db5928beaac83cc2e00e1bd3
GuiAz <guicottas@hotmail.com>
2025-12-29 18:12:53 -0300

    [Section 7 - Spring Data JPA e Integração da API ao MySQL] Ajustes do yaml para conexão com o banco de dados. Inicio do CRUD e postman.

--- 07_SpringDataJPAWithMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 07_SpringDataJPAWithMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -1,9 +1,10 @@
 package br.com.erudio.controllers;
 
 import br.com.erudio.model.Person;
-import br.com.erudio.request.converters.PersonServices;
+import br.com.erudio.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,58 +15,37 @@ public class PersonController {
 
     @Autowired
     private PersonServices service;
-    //Com o @Autowired e o @Service na classe PersonServices, não é mais necessário a injeção da dependencia,
-    // o Spring já faz isso com o @Autowired
-    //private PersonServices service = new PersonServices();
 
     // http://localhost:8080/person
-    //Requisicao do verbo GET para busca de todos as pessoas cadastradas
-    @RequestMapping(method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 
     public List<Person> findAll() {
         return service.findAll();
     }
 
     // http://localhost:8080/person/1
-    //Requisicao do verbo GET para busca de uma única pessoa (por ID)
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public Person findById(@PathVariable("id") String id) {
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
     // http://localhost:8080/person
-    //Requisicao do verbo POST para criação de uma pessoa no bando de dados
-    @RequestMapping(method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public Person create (@RequestBody Person person) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
         return service.create(person);
     }
 
     // http://localhost:8080/person
-    //Atualização do verbo PUT para Atualização de uma pessoa no bando de dados
-    @RequestMapping(method = RequestMethod.PUT,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public Person update (@RequestBody Person person) {
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
         return service.update(person);
     }
 
     // http://localhost:8080/person/1
-    //Requisicao do verbo DELETE para remoção de uma única pessoa (por ID) do banco de dados
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.DELETE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public void delete(String id) {
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
