75ca38d5e434af219dd4e8eeedce0c3566e1a585
GuiAz <guicottas@hotmail.com>
2025-12-31 14:49:53 -0300

    [09_WorkingWithDTOPattern] Padrao Dto, inicio do Dozer e inicio dos testes.

---
+++ 09_WorkingWithDTOPattern/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,51 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.data.dto.PersonDTO;
+import br.com.erudio.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    // http://localhost:8080/person
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+
+    public List<PersonDTO> findAll() {
+        return service.findAll();
+    }
+
+    // http://localhost:8080/person/1
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO findById(@PathVariable("id") Long id) {
+        return service.findById(id);
+    }
+
+    // http://localhost:8080/getAddress
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO create(@RequestBody PersonDTO person) {
+        return service.create(person);
+    }
+
+    // http://localhost:8080/person
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO update(@RequestBody PersonDTO person) {
+        return service.update(person);
+    }
+
+    // http://localhost:8080/person/1
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
