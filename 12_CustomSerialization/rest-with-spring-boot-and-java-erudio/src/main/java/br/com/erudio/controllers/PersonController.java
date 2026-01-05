00a6f4e43ae368731a140ad4e7e7b9ca7c37e08d
GuiAz <guicottas@hotmail.com>
2026-01-05 06:07:17 -0300

    [12_CustomSerialization] Alterando nomes, ordenacao e ignorando variaveis com JsonProperty, formatacao de datas com JsonFormat, serializacao com JsonSerializer e guardando dados sensiveis com JsonFilter

---
+++ 12_CustomSerialization/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,65 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.data.dto.PersonDTO;
+import br.com.erudio.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.crypto.Data;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    // http://localhost:8080/person
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+
+    public List<PersonDTO> findAll() {
+        return service.findAll();
+    }
+
+//    // http://localhost:8080/person/1
+//    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public PersonDTO findById(@PathVariable("id") Long id) {
+//        return service.findById(id);
+//    }
+
+    // http://localhost:8080/person/1
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO findById(@PathVariable("id") Long id) {
+        var person = service.findById(id);
+        person.setBirthDay(new Date());
+//        person.setPhoneNumber("+55 (34) 98765-4321");
+        person.setPhoneNumber("");
+        person.setLastName(null);
+        person.setSensitiveData("Foo Bar");
+        return person;
+    }
+
+    // http://localhost:8080/getAddress
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO create(@RequestBody PersonDTO person) {
+        return service.create(person);
+    }
+
+    // http://localhost:8080/person
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO update(@RequestBody PersonDTO person) {
+        return service.update(person);
+    }
+
+    // http://localhost:8080/person/1
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ 12_CustomSerialization/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/dto/PersonDTO.java
@@ -0,0 +1,128 @@
+package br.com.erudio.data.dto;
+
+import br.com.erudio.serializer.GenderSerializer;
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+//@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
+public class PersonDTO implements Serializable {
+
+    //    private static final long serialVersionID = 1L;
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    //    @JsonProperty("first_name")
+
+    private String firstName;
+    //    @JsonProperty("last_name")
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String lastName;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String phoneNumber;
+
+    @JsonFormat(pattern = "dd/MM/yyy")
+    private Date birthDay;
+
+    private String adress;
+    //    @JsonIgnore
+    @JsonSerialize(using = GenderSerializer.class)
+    private String gender;
+
+    @JsonFilter("PersonFilter")
+    private String sensitiveData;
+
+    public PersonDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return adress;
+    }
+
+    public void setAddress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSensitiveData() {
+        return sensitiveData;
+    }
+
+    public void setSensitiveData(String sensitiveData) {
+        this.sensitiveData = sensitiveData;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        PersonDTO personDTO = (PersonDTO) object;
+        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getPhoneNumber(), personDTO.getPhoneNumber()) && Objects.equals(getBirthDay(), personDTO.getBirthDay()) && Objects.equals(getAdress(), personDTO.getAdress()) && Objects.equals(getGender(), personDTO.getGender()) && Objects.equals(getSensitiveData(), personDTO.getSensitiveData());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getPhoneNumber(), getBirthDay(), getAdress(), getGender(), getSensitiveData());
+    }
+}
