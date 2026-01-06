e697b72241785273006c424aa375ea2db95a34c6
GuiAz <guicottas@hotmail.com>
2026-01-06 08:31:39 -0300

    [13_ContentNegotiation] retorno das applicacoes como XML, YAML e JSON. Criacao da WebConfig, YamlJackson e ajustes do PersonController para novas medias

--- 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -17,32 +17,48 @@ public class PersonController {
     private PersonServices service;
 
     // http://localhost:8080/person
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE}
+    )
 
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
     // http://localhost:8080/person/1
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}",
+            produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE}
+    )
     public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
     // http://localhost:8080/getAddress
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}
+    )
     public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
     // http://localhost:8080/person
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}
+    )
     public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
     }
 
     // http://localhost:8080/person/1
-    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
     public ResponseEntity<?> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
