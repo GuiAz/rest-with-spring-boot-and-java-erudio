780903f58d034d7e0b270989ea0836bcde27f15c
GuiAz <guicottas@hotmail.com>
2026-03-03 20:05:55 -0300

    [21_WorkingWithFiles] Criada Service

---
+++ 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/FileStorageService.java
@@ -0,0 +1,59 @@
+package br.com.erudio.services;
+
+import br.com.erudio.config.FileStorageConfig;
+import br.com.erudio.exception.FileStorageException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileStorageService {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileStorageService.class);
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        Path path = Paths.get(fileStorageConfig.getUploadDir())
+                .toAbsolutePath().normalize();
+
+        this.fileStorageLocation = path;
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+            logger.info("Creating Directories");
+        } catch (Exception e) {
+            logger.error("Could not create directory where files will be stored");
+            throw new FileStorageException("Could not create directory where files will be stored", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            if (fileName.contains("..")) {
+                logger.error("Sorry! Filename Contains a Invalid Sequence " + fileName);
+                throw new FileStorageException("Sorry! Filename Contains a Invalid Sequence " + fileName);
+            }
+
+            logger.info("Saving file in Disk");
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (Exception e) {
+            logger.error("Could not store file " + fileName + ". Plase try Again ");
+            throw new FileStorageException("Could not store file " + fileName + ". Plase try Again ", e);
+        }
+    }
+
+}
