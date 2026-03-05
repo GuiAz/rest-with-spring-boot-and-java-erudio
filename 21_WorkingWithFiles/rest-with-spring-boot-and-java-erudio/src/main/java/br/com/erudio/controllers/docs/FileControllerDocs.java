9aacb3344a3f2b5141336768e70e9a4fe5194586
GuiAz <guicottas@hotmail.com>
2026-03-04 21:32:20 -0300

    [21_WorkingWithFiles] Ajusta o arquivo de docs para uploadMultipleFiles e downloadFiles

--- 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/docs/FileControllerDocs.java
+++ 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/docs/FileControllerDocs.java
@@ -3,6 +3,7 @@ package br.com.erudio.controllers.docs;
 import br.com.erudio.data.dto.UploadFileResponseDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,6 +13,6 @@ import java.util.List;
 public interface FileControllerDocs {
 
     UploadFileResponseDTO uploadFile(MultipartFile file);
-    List<UploadFileResponseDTO> uploadFile(MultipartFile[] files);
-    ResponseEntity<ResponseEntity> downloadFile(String fileName, HttpServletRequest request);
+    List<UploadFileResponseDTO> uploadMultipleFile(MultipartFile[] files);
+    ResponseEntity<Resource> downloadFile(String fileName, HttpServletRequest request);
 }
