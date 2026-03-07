b1d4d87c46ed025c302ce2c8d7fb6a5c7977605c
GuiAz <guicottas@hotmail.com>
2026-03-07 17:35:48 -0300

    [22_WorkingWithCSVandXLSX] Creada classe de Medias para o Headers dos arquivos XLSX e CSV

---
+++ 22_WorkingWithCSVandXLSX/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/file/exporter/MediaTypes.java
@@ -0,0 +1,8 @@
+package br.com.erudio.file.exporter;
+
+public interface MediaTypes {
+
+    String APPLICATION_XLSX_VALUES = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    String APPLICATION_CSV_VALUES = "text/csv";
+
+}
