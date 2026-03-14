facb2baf2bc2bfb8d25c122bc1d476b8212ed89e
GuiAz <guicottas@hotmail.com>
2026-03-14 13:47:01 -0300

    [25_AuthenticationWithJWT] Implemtanda a DTO das credenciais

--- 25_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/dto/security/AccountCredentialsDTO.java
+++ 25_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/dto/security/AccountCredentialsDTO.java
@@ -9,8 +9,16 @@ public class AccountCredentialsDTO implements Serializable {
 
     private String username;
     private String password;
+    private String fullname;
 
-    public AccountCredentialsDTO() {}
+    public AccountCredentialsDTO() {
+    }
+
+    public AccountCredentialsDTO(String username, String password, String fullname) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+    }
 
     public String getUsername() {
         return username;
@@ -28,15 +36,23 @@ public class AccountCredentialsDTO implements Serializable {
         this.password = password;
     }
 
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
     @Override
     public boolean equals(Object object) {
         if (object == null || getClass() != object.getClass()) return false;
         AccountCredentialsDTO that = (AccountCredentialsDTO) object;
-        return Objects.equals(getUsername(), that.getUsername()) && Objects.equals(getPassword(), that.getPassword());
+        return Objects.equals(getUsername(), that.getUsername()) && Objects.equals(getPassword(), that.getPassword()) && Objects.equals(getFullname(), that.getFullname());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getUsername(), getPassword());
+        return Objects.hash(getUsername(), getPassword(), getFullname());
     }
 }
