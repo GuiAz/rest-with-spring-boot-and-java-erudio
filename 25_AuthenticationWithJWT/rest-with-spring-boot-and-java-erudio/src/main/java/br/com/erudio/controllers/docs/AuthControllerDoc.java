bbfa4bf2f887355d3d2216c67165259ce56b3190
GuiAz <guicottas@hotmail.com>
2026-03-14 13:48:25 -0300

    [25_AuthenticationWithJWT] controler

--- 25_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/docs/AuthControllerDoc.java
+++ 25_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/docs/AuthControllerDoc.java
@@ -1,4 +1,16 @@
 package br.com.erudio.controllers.docs;
 
+import br.com.erudio.data.dto.security.AccountCredentialsDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+
 public interface AuthControllerDoc {
+
+    @Operation(summary = "Authenticates an user and returns a token")
+    ResponseEntity<?> signIn(AccountCredentialsDTO credentials);
+
+    @Operation(summary = "Refresh token for authenticated user and returns a new token")
+    ResponseEntity<?> refreshToken(String username, String refreshToken);
+
+    AccountCredentialsDTO create(AccountCredentialsDTO credentials);
 }
