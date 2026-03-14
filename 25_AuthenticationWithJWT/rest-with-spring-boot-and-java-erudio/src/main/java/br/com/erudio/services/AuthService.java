ecf504b527eeb1e75de6f88cdf1df4699b31619e
GuiAz <guicottas@hotmail.com>
2026-03-14 13:48:01 -0300

    [25_AuthenticationWithJWT] Implementada a classe service de auth

--- 25_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/AuthService.java
+++ 25_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/AuthService.java
@@ -2,6 +2,8 @@ package br.com.erudio.services;
 
 import br.com.erudio.data.dto.security.AccountCredentialsDTO;
 import br.com.erudio.data.dto.security.TokenDTO;
+import br.com.erudio.exception.RequiredObjectIsNullException;
+import br.com.erudio.model.User;
 import br.com.erudio.repository.UserRepository;
 import br.com.erudio.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 @Service
 public class AuthService {
@@ -20,6 +29,8 @@ public class AuthService {
     private JwtTokenProvider tokenProvider;
     @Autowired
     private UserRepository repository;
+
+    Logger logger = Logger.getLogger(AuthService.class.getName());
 
     public ResponseEntity<TokenDTO> signIn(AccountCredentialsDTO credentials) {
         authenticationManager.authenticate(
@@ -43,5 +54,49 @@ public class AuthService {
         return ResponseEntity.ok(tokenResponse);
     }
 
+    public ResponseEntity<TokenDTO> refreshToken(String username, String refreshToken) {
+        var user = repository.findByUsername(username);
+        TokenDTO token;
+        if (user != null) {
+            token = tokenProvider.refreshToken(refreshToken);
+        } else {
+            throw new UsernameNotFoundException("Username " + username + " not found!");
+        }
+        return ResponseEntity.ok(token);
+    }
+
+    private String generateHashedPassword(String password) {
+
+        PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder(
+                "", 8, 185000,
+                Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("pbkdf2", pbkdf2Encoder);
+
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+        return passwordEncoder.encode(password);
+    }
+
+    public AccountCredentialsDTO create(AccountCredentialsDTO user) {
+
+        if (user == null) throw new RequiredObjectIsNullException();
+
+        logger.info("Creating one new User!");
+        var entity = new User();
+
+        entity.setFullName(user.getFullname());
+        entity.setUserName(user.getUsername());
+        entity.setPassword(generateHashedPassword(user.getPassword()));
+        entity.setAccountNonExpired(true);
+        entity.setAccountNonLocked(true);
+        entity.setCredentialsNonExpired(true);
+        entity.setEnabled(true);
+
+        var dto = repository.save(entity);
+        return new AccountCredentialsDTO(dto.getUsername(), dto.getPassword(), dto.getFullName());
+    }
 
 }
